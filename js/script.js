76985bee1a75c104b55dfd64571f94b6a5c102d7
150Victor150 <victor.silva.oliveira@escola.pr.gov.br>
2025-06-23 11:30:51 -0300

    fiz o commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+let estoque = 200;
+
+
+function comprar(){
+
+
+    let compra_venda = 0;
+
+   
+    const nome_cliente = document.getElementById("nome").value;
+    const nome_produto = document.getElementById("produto").value;
+    const quantidade_produto = document.getElementById("quantidade").value;
+
+    if (quantidade_produto > 200){
+        alert ("acabou o estoque");
+        
+    } else {
+        alert ("poderá efetuar a compra");
+    }
+
+    
+       
+    
+
+
+    
+      
+    
+
+
+
+}
+
+
