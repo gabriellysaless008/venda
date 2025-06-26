1c26de6d289b90d646e0cdf351c84e767f38b9ca
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2025-06-26 15:54:20 -0300

    c

--- js/script.js
+++ js/script.js
@@ -1,29 +1,31 @@
+
 let estoque = 7;
 
-function comprar(){
-    for ( let contador = 0; contador < 3; contador++ ){
+ function comprar(){
+     for ( let contador = 0; contador < 3; contador++ ){
     
-        const nome_cliente = document.getElementById( "nome" ).value;
-        console.log ( nome_cliente );
+         const nome_cliente = document.getElementById( "nome" ).value;
+         console.log ( nome_cliente );
 
-        const nome_produto = document.getElementById( "produto" ).value;
-        console.log ( nome_produto );
+         const nome_produto = document.getElementById( "produto" ).value;
+         console.log ( nome_produto );
 
-        const quantidade_vendida = document.getElementById( "quantidade" ).value;
-        console.log ( quantidade_vendida );
+         const quantidade_vendida = document.getElementById( "quantidade" ).value;
+         console.log ( quantidade_vendida );
 
-        if (quantidade_vendida > estoque){
-            console.log( "acabou o estoque" );
+         if (quantidade_vendida > estoque){
+             alert( "acabou o estoque" );
             
-        } else {
-            consoole.log( "sua compra foi efetuada com sucesso!" );
-            estoque = estoque - quantidade_vendida;
+         } else {
+            alert( "sua compra foi efetuada com sucesso!" );
+             estoque = estoque - quantidade_vendida;
 
         }
 
-        console.log(contador)
+        
 
-    }
-}
+     }
+      console.log(contador)
+ }
 
 
